9faaa5f54439fa58b47e1204ee9349848c46580a
frozenegg <silicondoo@gmail.com>
2020-02-16 21:37:26 +0900

    add new client file

---
+++ python_script/tcp_ip/input_recursive_client.py
@@ -0,0 +1,19 @@
+import socket
+
+ip_input = input('ip\n >> ')
+port_input = input('port\n >> ')
+
+while True:
+
+    message_raw_input = input('message\n >> ')
+    message_input = message_raw_input.encode()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #determine server
+        s.connect((ip_input, int(port_input)))
+        #send server a message
+        s.sendall(message_input)
+        #buffer size of the network is 1024
+        #receive data from server
+        data = s.recv(1024)
+        print(repr(data))
